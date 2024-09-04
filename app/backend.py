7f457e83290dae770a084f9c2a207c217d97c205
Romulo Balsalobre <93260053+RomuloBalsalobre@users.noreply.github.com>
2024-09-04 20:19:27 -0300

    adicionado pagina quem somos, alterações no front (#13)
    end e filtro do backend
    
    Co-authored-by: Romulo Balsalobre <romulobalsalbre@gmail.com>

--- app/backend.py
+++ app/backend.py
@@ -2,10 +2,7 @@
 from flask import Flask, request, render_template_string,render_template,flash,redirect,url_for, session
 import requests
 from unidecode import unidecode
-
-
 import mysql.connector
-
 
 db = mysql.connector.connect(
     host="localhost",
@@ -30,51 +27,49 @@ def reportincident():
         obs = request.form.get('obs')
         escolaridade = request.form.get('escolaridade')
 
-
+        #verifica se o endereço digitado é valido
         UF = 'SP'
+        #caso o usuario nao digite a rua, usa um valor padrao
+        if rua == '' or len(rua) < 3:
+            rua = 'teste'
         response = requests.get(f'https://viacep.com.br/ws/{UF}/diadema/{rua}/json/')
         data = response.json()
         #mostra o json recebido
         print(data)
         
-
-        
+        #verifica se o json recebido é vazio
         if not data:
-            flash('Endereço inválido')
+            flash('Rua ou bairro inválido - OBS: Não digite o número da casa, apenas o nome da rua')
+            session['nome'] = request.form.get('nome')
+            session['rua'] = request.form.get('rua')
+            session['bairro'] = request.form.get('bairro')
+            session['periodo'] = request.form.get('periodo')
+            session['idade'] = request.form.get('idade')
+            session['obs'] = request.form.get('obs')
+            session['escolaridade'] = request.form.get('escolaridade')
             nome = session.get('nome')
             rua = session.get('rua')
             bairro = session.get('bairro')
             periodo = session.get('periodo')
             idade = session.get('idade')
             obs = session.get('obs')
-            escolaridade = session.get('escolaridade')
-            #recarrega a pagina inicial
-            
-        #verifica se a rua digitada bate igual a rua do json e se o bairro escolhido tbm bate
+            escolaridade = session.get('escolaridade')      
         
+        
+        #verifica se o endereço digitado é igual ao endereço do json recebido
         elif unidecode(rua).upper() == unidecode(data[0]['logradouro'].upper()) and unidecode(bairro) == unidecode(data[0]['bairro']):
             flash('Endereço salvo com sucesso!', 'success')
             mycursor = db.cursor()
-            sql = "INSERT INTO assaltos (nome, rua, bairro, periodo, idade, obs, escolaridade) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            sql = "INSERT INTO assaltos (nome, rua, bairro, periodo, idade, obs, escolaridade, data) VALUES (%s, %s, %s, %s, %s, %s, %s, NOW())"
             val = (nome, rua.upper(), bairro, periodo, idade, obs, escolaridade)
             mycursor.execute(sql, val)
             db.commit()
             print(mycursor.rowcount, "record inserted.")
-            session['nome'] = request.form.get('nome')
-            session['rua'] = request.form.get('rua')
-            session['bairro'] = request.form.get('bairro')
-            session['periodo'] = request.form.get('periodo')
-            session['idade'] = request.form.get('idade')
-            session['obs'] = request.form.get('obs')
-            session['escolaridade'] = request.form.get('escolaridade')
-            nome = session.get('nome')
-            rua = session.get('rua')
-            bairro = session.get('bairro')
-            periodo = session.get('periodo')
-            idade = session.get('idade')
-            obs = session.get('obs')
-            escolaridade = session.get('escolaridade')
+            
+        
+        #caso o endereço digitado seja diferente do endereço do json recebido
         else:
+            flash('Rua ou bairro inválido - OBS: Não digite o número da casa, apenas o nome da rua')
             session['nome'] = request.form.get('nome')
             session['rua'] = request.form.get('rua')
             session['bairro'] = request.form.get('bairro')
@@ -82,27 +77,28 @@ def reportincident():
             session['idade'] = request.form.get('idade')
             session['obs'] = request.form.get('obs')
             session['escolaridade'] = request.form.get('escolaridade')
-            flash('Endereço inválido')
             nome = session.get('nome')
             rua = session.get('rua')
             bairro = session.get('bairro')
             periodo = session.get('periodo')
             idade = session.get('idade')
             obs = session.get('obs')
-            escolaridade = session.get('escolaridade')
+            escolaridade = session.get('escolaridade')      
+        
    
     with open("static\HTML\PaginaInicial.html", encoding='utf-8') as file:
         template = file.read()
+
     return render_template_string(template, nome=session.get('nome', ''), rua=session.get('rua', ''), bairro=session.get('bairro', ''), periodo=session.get('periodo', ''), idade=session.get('idade', ''), obs=session.get('obs', ''), escolaridade=session.get('escolaridade', ''))
 
 @app.route('/quemSomos', methods=['GET','POST'])
 def quemSomos():
     return render_template_string(open("static\HTML\quemSomos.html", encoding='utf-8').read())
 
+
 @app.route('/consulta', methods=['GET','POST'])
 def consulta():
     mycursor = db.cursor()
-
     # Obter os parâmetros de consulta do request
     bairro = request.args.get('bairro')
     periodo = request.args.get('periodo')
@@ -123,10 +119,28 @@ def consulta():
         # Se nenhum bairro ou período foram fornecidos, selecione todos os registros incluindo uma coluna de quantidade de vezes que o endereço foi registrado e deixe o que tiver mais registros em cima        
         query = "SELECT rua,bairro,periodo, COUNT(*) as quantidade FROM assaltos GROUP BY rua,bairro,periodo order by quantidade desc"
         mycursor.execute(query)
-
+        
     dados = mycursor.fetchall()
+    
+    # Contar o total de casos
+    query = "SELECT COUNT(*) FROM assaltos"
+    mycursor.execute(query)
+    total_casos = int(mycursor.fetchone()[0])
+    
+    #salva na variavel data_atualizacao a data salva no banco de dados da ultima linha convertendo para dd-mm-aaaa
+    query = "SELECT data FROM assaltos ORDER BY data DESC LIMIT 1"
+    mycursor.execute(query)
+    data_atualizacao = mycursor.fetchone()[0]
+    data_atualizacao = data_atualizacao.strftime('%d-%m-%Y')
+    
     html_string = open("static\HTML\consulta.html", encoding='utf-8').read()
-    return render_template_string(html_string, dados=dados)
+    return render_template_string(html_string, dados=dados, total_casos=total_casos, data_atualizacao=data_atualizacao)
 
+@app.route('/quemSomos', methods=['GET','POST'])
+def quemsomos():
+    with open("static\HTML\quemsomos.html", encoding='utf-8') as file:
+        template = file.read()
+    return render_template_string(template)
+  
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
